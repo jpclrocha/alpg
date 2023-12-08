678c33928b855c97689306c5039ce9ac3fa1bba3
jpclrocha <jpclrocha@gmail.com>
2023-12-08 00:22:31 -0300

    Question 7

---
+++ exercises02/question7.py
@@ -0,0 +1,7 @@
+comprimento = float(input("Digite a comprimento do retangulo: "))
+
+largura = float(input("Digite a largura do retangulo: "))
+
+area = comprimento * largura
+
+print(f"A area do retangulo eh {area}")
